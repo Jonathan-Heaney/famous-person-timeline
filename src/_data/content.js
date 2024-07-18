a339c33f7aff3ee9c7178b3474e4d85e035ae179
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-07-17 20:01:56 -0500

    Add Martin Luther

--- src/_data/content.js
+++ src/_data/content.js
@@ -1,20 +1,20 @@
-const header = 'Welcome to this wonderful example timeline.';
+const header = 'Famous People Throughout History';
 const footer = 'A footer goes here.';
 const entries = [
   {
-    id: 'cat',
-    categories: ['cat'],
+    id: 'martinLuther',
+    categories: ['Religious Figure', '1400s'],
     color: 'green',
-    faicon: 'cat',
-    datetime: '2021-01-01 05:00',
-    title: 'Cats are very good',
+    faicon: 'church',
+    datetime: '1483-11-10',
+    title: 'Martin Luther',
     image: {
-      link: 'http://placekitten.com',
-      src: 'http://placekitten.com/200/300',
-      alt: 'A placeholder kitten',
-      caption: 'Kitten!',
+      link: 'https://commons.wikimedia.org/wiki/File:Lucas_Cranach_d.%C3%84._-_Martin_Luther,_1528_(Veste_Coburg)_(cropped).jpg',
+      src: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Lucas_Cranach_d.%C3%84._-_Martin_Luther%2C_1528_%28Veste_Coburg%29_%28cropped%29.jpg',
+      alt: 'Martin Luther',
+      caption: 'Martin Luther',
     },
-    body: "The best thing in the universe is a cardboard box. Chirp at birds catasstrophe for meowwww for hiding behind the couch until lured out by a feathery toy for morning beauty routine of licking self sugar, my siamese, stalks me (in a good way), day and night lick the other cats. Pushed the mug off the table shove bum in owner's face like camera lens or tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad and grass smells good, licks your face, but hiiiiiiiiii feed me now.",
+    body: 'Martin Luther started the Protestant Reformation.',
     links: [
       {
         href: 'https://en.wikipedia.org/wiki/Cat',
